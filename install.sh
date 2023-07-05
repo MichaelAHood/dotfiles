fcd2c2b1944de641e3459497494b4efba8dde113
Michael Hood <7614815+MichaelAHood@users.noreply.github.com>
2023-07-05 15:35:18 -0700

    Checking in various setup scripts and config files

---
+++ install.sh
@@ -0,0 +1,22 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install curl
+brew install coreutils
+brew install binutils
+brew install diffutils
+brew install jq
+brew install ed --with-default-names
+brew install findutils --with-default-names
+brew install gawk
+brew install gnu-indent --with-default-names
+brew install gnu-sed --with-default-names
+brew install gnu-tar --with-default-names
+brew install gnu-which --with-default-names
+brew install gnutls
+brew install grep --with-default-names
+brew install gzip
+brew install screen
+brew install watch
+brew install wdiff --with-gettext
+brew install wget
+brew install ag
